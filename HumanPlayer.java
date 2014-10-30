6f15b5c20bfcfdfd8fb9d16d699951224b4846d1
Steinunn <stemfri@gmail.com>
2014-10-30 14:26:49 +0000

    implemented newgame and human player constructor

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -3,8 +3,17 @@ package TicTacToe;
 public class HumanPlayer {
 	private int numberOfWins = 0;
 	
+	public HumanPlayer(char token) {
+		score() = 0;
+		token = '\0';
+	}
+
 	public void takeTurn(Board board) {
 		
+	}
+
+	public int score() {
+
 	}
 }
 
