a149b3ce88bbc537d225e1e94f43bc9e44a9ec99
fanney hrund jÃjonasdottir <fanneyhrund@i-b9db7e1e.greenqloud.internal>
2014-10-30 11:47:45 +0000

    added boardfull function

--- Board.java
+++ Board.java
@@ -14,5 +14,16 @@ public class Board {
 	public void updateBoard(int x, int y, HumanPlayer p) {
 		
 	}
+	public boolean boardIsFull(){
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				if(board[i][j] == ' '){
+					return false;
+				}
+			}
+		}
+		return true;
+
+	}
 }
 
