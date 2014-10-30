c1bba280afd6b1647a6ffa016a41dc2a37addfb4
Erla Hardardottir <erla1011@i-b9db7e1e.greenqloud.internal>
2014-10-30 17:45:30 +0000

    added getWins function in HumanPlayer.java

--- src/main/java/is/ru/TicTacToe/HumanPlayer.java
+++ src/main/java/is/ru/TicTacToe/HumanPlayer.java
@@ -1,22 +1,23 @@
 package is.ru.TicTacToe;
 
 public class HumanPlayer {
-	private int numberOfWins = 0;
-	
+	private int numberOfWins;
+
 	public HumanPlayer(char token) {
 		if(token != 'X' && token != 'O'){
 			throw new IllegalArgumentException("Please Insert either X or O");
 		}
-		int score = 0;
+		numberOfWins = 0;
 		token = token;
 	}
 
 	public void takeTurn(Board board) {
-		
+
 	}
 
-	public int score() {
-		return 0;
+	public int getWins() {
+		return numberOfWins;
 	}
+
 }
 
