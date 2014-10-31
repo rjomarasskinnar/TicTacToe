4045d0a07caed03056f17d99137d8ed7681aee82
Steinunn Marta Friðriksdóttir <steinunnmf@i-b9db7e1e.greenqloud.internal>
2014-10-31 12:56:57 +0000

    fixed problems in humanplayer and board

--- src/main/java/is/ru/TicTacToe/HumanPlayer.java
+++ src/main/java/is/ru/TicTacToe/HumanPlayer.java
@@ -1,40 +1,41 @@
 package is.ru.TicTacToe;
+import java.util.Scanner;
 
 public class HumanPlayer {
-	private int numberOfWins;
-	private char token;
+        private int numberOfWins;
+        private char token;
 
-	public HumanPlayer(char token) {
-		if(token != 'X' && token != 'O'){
-			throw new IllegalArgumentException("Please Insert either X or O");
-		}
-		this.numberOfWins = 0;
-		this.token = token;
-	}
+        public HumanPlayer(char token) {
+                if(token != 'X' && token != 'O'){
+                        throw new IllegalArgumentException("Please Insert either X or O");
+                }
+                this.numberOfWins = 0;
+                this.token = token;
+        }
 
-	public void takeTurn(Board board[][], HumanPlayer p) {
-		boolean validInput = false;
-		while (!validInput) {
-			System.out.println("Player " + p.token + " please choose a box (1-9): ");
-			int boxno = System.in.read();
-			boxno--;
-			if (board[boxno / 3][boxno % 3] == ' ') {
+        public void takeTurn(Board board, HumanPlayer p) {
+                boolean validInput = false;
+                while (!validInput) {
+                        Scanner in = new Scanner(System.in);
+                        System.out.println("Player " + p.getToken() + " please choose a box (1-9): ");
+                        int boxno = in.nextInt();
+                        if (boxno <=1 && boxno <= 9)
+                        {
+                                boxno = boxno - 1;
+                                board.updateBoard(boxno , p);
+                                validInput = true;
+                        }
 
-				board[boxno / 3][boxno % 3] = p.token;
-				validInput = true;
-			
-			} else {
-				System.out.println("This box is already taken!");
-			}
-		}
-	}
+                }
+        }
 
-	public int getWins() {
-		return numberOfWins;
-	}
+        public int getWins() {
+                return numberOfWins;
+        }
 
-	public char getToken() {
-		return token;
-	}
+        public char getToken() {
+                return token;
+        }
 }
+
 
